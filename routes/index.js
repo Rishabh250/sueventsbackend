1ccc13a810446203692dde0a71dc861df91b2e1d
Rishabh250 <rishu25bansal@gmail.com>
2022-05-14 12:26:15 +0530

    rishabh

--- routes/index.js
+++ routes/index.js
@@ -4,65 +4,62 @@ const events = require('../methods/eventsActions');
 const router = express.Router();
 
 
-router.get('',(req,res)=>{
+router.get('/connection', (req, res) => {
     res.send("Connected");
 
 });
-router.get('/user',(req,res)=>{
-    res.send("Rishabh");
 
-});
 
 //Register User
-router.post("/createUser",actions.addNew);
+router.post("/createUser", actions.addNew);
 
 //Login User
-router.post("/loginUser",actions.authorization);
+router.post("/loginUser", actions.authorization);
 
 //Get Single User
-router.get("/getSingleUser",actions.singleUser);
+router.get("/getSingleUser", actions.singleUser);
 
 // Post user Image
-router.post("/uploadImage",actions.uploadImage);
+router.post("/uploadImage", actions.uploadImage);
 
 //Get Single User Info
-router.get("/userInfo",actions.getUserInfo);
+router.get("/userInfo", actions.getUserInfo);
 
 //Get All User Data
-router.get("/getAllUser",actions.getAllUser);
+router.get("/getAllUser", actions.getAllUser);
 
 //Forget Password
-router.post("/forgetPassword",actions.forgetPassword);
+router.post("/forgetPassword", actions.forgetPassword);
 
 //Create Event
-router.post("/createEvent",events.createEvent);
+router.post("/createEvent", events.createEvent);
 
 //Get All Events
-router.get("/getAllEvents",events.getAllEvents);
+router.get("/getAllEvents", events.getAllEvents);
 
 //Store Rounds
-router.post("/createRound",events.createRounds);
+router.post("/createRound", events.createRounds);
 
 //Send OTP
-router.post("/sendOTP",actions.sendOTP);
+router.post("/sendOTP", actions.sendOTP);
 
 //Verify OTP
-router.post("/verifyOTP",actions.verifyOTP);
+router.post("/verifyOTP", actions.verifyOTP);
 
 //Student Apply
-router.post("/applyEvent",events.applyEvent);
+router.post("/applyEvent", events.applyEvent);
 
 //Selected Students or Attendence
-router.post("/selectedStudents",events.selectedStudends);
+router.post("/selectedStudents", events.selectedStudends);
 
 //Event Round
-router.post("/getEventRound",events.getEventRound);
+router.post("/getEventRound", events.getEventRound);
 
 //Single Round
-router.post("/getSingleRound",events.getSingletRound);
+router.post("/getSingleRound", events.getSingletRound);
 
 //Close Event
-router.post("/closeEvent",events.closeEvent);
+router.post("/closeEvent", events.closeEvent);
 
 
 
