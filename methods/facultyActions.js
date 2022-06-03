a1e5f8d83edef30740367514d7383567a05953ef
Rishabh250 <rishu25bansal@gmail.com>
2022-06-03 16:30:12 +0530

    rishabh

--- methods/facultyActions.js
+++ methods/facultyActions.js
@@ -323,18 +323,17 @@ var functions = {
             console.log(eventID)
             let getEvent = await Events.findOne({_id: eventID});
             console.log(getEvent)
-            // if (getEvent.status === "open") {
-            //     let facultyList = req.body.facultyID
-            //     for(let i=0; i<facultyList.length ; i++  ){
-            //         await getEvent.facultyAssigned.push(facultyList[i]);
-            //     }
-                
-            // } else {
-            //     return res.status(400);
+            if (getEvent.status === "open") {
+                let facultyList = req.body.facultyID
+                for(let i=0; i<facultyList.length ; i++  ){
+                    await getEvent.facultyAssigned.push(facultyList[i]);
+                }
+                await getEvent.save();
+                return res.status(200).json(getEvent);
+            } else {
+                return res.status(400);
     
-            // }
-            await getEvent.save();
-            return res.status(200).json(getEvent);
+            }
     
         }    
         catch(e){
